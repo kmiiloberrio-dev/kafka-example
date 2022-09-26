e00c45ee0fca175705fabf85f1666ab06cf97b87
kmiiloberrio-dev <kmiiloberrio@gmail.com>
2022-09-26 10:58:04 -0500

    Add all messages from beginning in listener

--- src/main/kotlin/com/example/kafkaexample/kafka/listeners/HelloKafkaListener.kt
+++ src/main/kotlin/com/example/kafkaexample/kafka/listeners/HelloKafkaListener.kt
@@ -1,14 +1,19 @@
 package com.example.kafkaexample.kafka.listeners
 
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.PartitionOffset
+import org.springframework.kafka.annotation.TopicPartition
 import org.springframework.stereotype.Component
 
 @Component
 class HelloKafkaListener {
 
     @KafkaListener(
-        topics = ["local-kafka-topic"],
-        groupId = "com.example.kafkaexample.kafka.HelloKafkaListener"
+        id = "receiver-api",
+        topicPartitions = [TopicPartition(
+            topic = "local-kafka-topic",
+            partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "0")]
+        )]
     )
     fun listener(data: String) {
         println("Listener received $data \uD83C\uDF89")
